a398fefa3a212f67a37194c9fe9f1de3f9184539
Johnson Pan <panjunsheng@msn.com>
2016-05-20 00:38:06 -0700

    Added IP area

---
+++ InfoWeb/InfoWeb/Areas/IP/Controllers/Api/IPController.cs
@@ -0,0 +1,43 @@
+﻿using InfoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Channels;
+using System.Web;
+using System.Web.Http;
+
+namespace InfoWeb.Areas.IP.Controllers.Api
+{
+    public class IPController : ApiController
+    {
+        // GET: api/ip
+        public string Get()
+        {
+            return this.SmartWebReturn(GetClientIp());
+        }
+        private string GetClientIp(HttpRequestMessage request = null)
+        {
+            request = request ?? Request;
+
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }
+            else if (request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty prop = (RemoteEndpointMessageProperty)request.Properties[RemoteEndpointMessageProperty.Name];
+                return prop.Address;
+            }
+            else if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ InfoWeb/InfoWeb/Areas/IP/IPAreaRegistration.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace InfoWeb.Areas.IP
+{
+    public class IPAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "IP";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+
+            context.Routes.MapHttpRoute(
+                name: "IPApiAction",
+                routeTemplate: "ip/api/{controller}/{action}",
+                // Optional: Added the following line to support default /ip/api access
+                defaults: new { action = "Get", area = AreaName, controller = AreaName, id = UrlParameter.Optional }
+            );
+
+            context.Routes.MapHttpRoute(
+                name: "IPApi",
+                routeTemplate: "ip/api/{controller}",
+                // Optional: Added the following line to support default /ip/api access
+                defaults: new { action = "Get", area = AreaName, controller = AreaName, id = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
+                "IP_defaultAction_lang",
+                "{lang}/ip/{controller}/{action}/{id}",
+                constraints: new { lang = @"(\w{2})|(\w{2}-\w{2})" },
+                defaults: new { action = "Index", area = AreaName, controller = AreaName, id = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
+                "IP_defaultAction",
+                "ip/{controller}/{action}/{id}",
+                new { action = "Index", area = AreaName, controller = AreaName, id = UrlParameter.Optional }
+            );
+
+            //context.MapRoute(
+            //    "IP_default",
+            //    "ip/{controller}/{action}/{id}",
+            //    new { action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
